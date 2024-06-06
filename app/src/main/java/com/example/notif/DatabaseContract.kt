3e9133d9a1435cc4dccca28c6d391961bee71838
Stingray <stingraysonggay@gmail.com>
2024-06-06 09:57:12 +0800

    Created DB Contract

--- app/src/main/java/com/example/notif/DatabaseContract.kt
+++ app/src/main/java/com/example/notif/DatabaseContract.kt
@@ -2,14 +2,25 @@ package com.example.notif
 import android.provider.BaseColumns
 
 object DatabaseContract {
-    object ExampleEntry: BaseColumns {
-        const val TABLE_NAME = "chat"
+    object Conversation: BaseColumns {
+        const val TABLE_NAME = "conversation"
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_CONVERSATION_NAME = "conversationName"
+        const val COLUMN_NAME_IS_GC = "isGC"
+        const val COLUMN_NAME_PLATFORM = "platform"
+    }
+
+    object Message: BaseColumns {
+        const val TABLE_NAME = "message"
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_CONTENT = "content"
+        const val COLUMN_NAME_SENDER = "sender"
+        const val COLUMN_NAME_CONVERSATION = "conversation"
+    }
+
+    object User: BaseColumns {
+        const val TABLE_NAME = "user"
+        const val COLUMN_NAME_ID = "id"
+        const val COLUMN_NAME_USERNAME = "username"
     }
 }
-
-// Conversation
-// ID            ConvName          IsGC?       Platform
-// Message
-// ID            Content           Sender(User)      Conversation(Conversation)
-// User
-// ID            Username
