29b0ff9c57641c8b20e7a03c4ab9bb912c380bba
Stingray <stingraysonggay@gmail.com>
2024-06-18 20:21:39 +0800

    Function Decomposition on onNotificationPosted

--- app/src/main/java/com/example/notif/MyNotificationListenerService.kt
+++ app/src/main/java/com/example/notif/MyNotificationListenerService.kt
@@ -4,75 +4,78 @@ import android.app.Notification
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
-import java.lang.StringBuilder
+
 
 class MyNotificationListenerService : NotificationListenerService() {
+
+    private lateinit var dbHelper: DatabaseHelper
+
+    override fun onCreate() {
+        super.onCreate()
+        dbHelper = DatabaseHelper(this)
+    }
+
     override fun onNotificationPosted(sbn: StatusBarNotification) {
         super.onNotificationPosted(sbn)
 
-        val INSTAGRAM_PACKAGE_NAME = "com.instagram.android"
-        val MESSENGER_PACKAGE_NAME = "com.facebook.orca"
+        val db = dbHelper.writableDatabase
 
-        val notification = sbn.notification
-        val extras = notification.extras
-        val conversationName = extras.getString(Notification.EXTRA_TITLE)
-        val tickerText = sbn.notification.tickerText?.toString()
-        val packageName = sbn.packageName
+        try {
+            val notification = sbn.notification
+            val extras = notification.extras
+            val conversationName = extras.getString(Notification.EXTRA_TITLE)
+            val tickerText = sbn.notification.tickerText?.toString()
+            val packageName = sbn.packageName
 
-        if (conversationName == null || tickerText == null) {
-            return
-        }
-
-        val sender: StringBuilder
-        val platform: String
-        val isGC: Int
-
-        when (packageName) {
-            INSTAGRAM_PACKAGE_NAME -> {
-                platform = "INSTAGRAM"
-                sender = StringBuilder(conversationName)
-            }
-
-            MESSENGER_PACKAGE_NAME -> {
-                platform = "MESSENGER"
-                sender = getSender(tickerText)
-            }
-
-            else -> {
-                println("Package Not Found")
+            if (conversationName == null || tickerText == null) {
                 return
             }
+
+            val (sender, platform) = getSenderAndPlatform(packageName, conversationName, tickerText) ?: return
+            val isGC = if (isFromGroupChat(sender, conversationName)) 1 else 0
+            println(isGC)
+
+            Log.d("NotificationListener", "Conversation Name: $conversationName")
+            Log.d("NotificationListener", "Message Sender: $sender")
+            Log.d("NotificationListener", "Notification TickerText: $tickerText")
+
+//            dbHelper.insertMessage(db, conversationName, sender, platform, isGC, tickerText)
+        } finally {
+            db.close()
         }
-
-        if (isFromGroupChat(sender, conversationName)) {
-            isGC = 1
-        }
-
-        Log.d("NotificationListener", "Conversation Name: $conversationName")
-        Log.d("NotificationListener", "Message Sender: $sender")
-        Log.d("NotificationListener", "Notification TickerText: $tickerText")
-
     }
 
     override fun onNotificationRemoved(sbn: StatusBarNotification) {
         println("onNotificationRemoved")
     }
 
-    private fun getSender(tickerText: String): StringBuilder {
+    private fun getSender(tickerText: String): String {
         val sender = StringBuilder("")
         for (char in tickerText) {
             if (char == ':') {
-                return sender
+                break
             }
             sender.append(char)
         }
-        return sender
+        return sender.toString()
     }
 
-    private fun isFromGroupChat(sender: StringBuilder, title: String): Boolean {
-        val stringSender = sender.toString()
-        return stringSender != title
+    private fun isFromGroupChat(sender: String, title: String): Boolean {
+        return sender != title
     }
 
-
+    private fun getSenderAndPlatform(packageName: String, conversationName: String, tickerText: String): Pair<String, String>? {
+        return when (packageName) {
+            "com.instagram.android" -> {
+                Pair(conversationName, "INSTAGRAM")
+            }
+            "com.facebook.orca" -> {
+                Pair(getSender(tickerText), "MESSENGER")
+            }
+            else -> {
+                Log.d("NotificationListener", "Package Not Found")
+                null
+            }
+        }
+    }
 }
